b21616f8121ade0cabbfea5493fb6d3c7a0cd90a
Aleh Haidash <aleh.haidash@instinctools.ru>
2019-07-26 12:36:51 +0300

    initialized services

---
+++ RegistrationServerService/src/main/java/by/haidash/shop/server/ServiceRegistrationServer.java
@@ -0,0 +1,14 @@
+package by.haidash.shop.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistrationServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistrationServer.class, args);
+    }
+}
